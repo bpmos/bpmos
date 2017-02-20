570e8829f8c9f0f6188794d9d6920409102713d1
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2017-02-20 13:29:31 -0300

    vscode plugin client and server

---
+++ tools/plugins/vscode-bpmos-client/server/server.js
@@ -0,0 +1,51 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+const vscode_languageserver_1 = require("vscode-languageserver");
+// Create a connection for the server. The connection uses Node's IPC as a transport
+let connection = vscode_languageserver_1.createConnection(new vscode_languageserver_1.IPCMessageReader(process), new vscode_languageserver_1.IPCMessageWriter(process));
+// Create a simple text document manager. The text document manager
+// supports full document sync only
+let documents = new vscode_languageserver_1.TextDocuments();
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+// After the server has started the client sends an initialize request. The server receives
+// in the passed params the rootPath of the workspace plus the client capabilities. 
+let workspaceRoot;
+connection.onInitialize((params) => {
+    workspaceRoot = params.rootPath;
+    return {
+        capabilities: {
+            // Tell the client that the server works in FULL text document sync mode
+            textDocumentSync: documents.syncKind
+        }
+    };
+});
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent((change) => {
+    let diagnostics = [];
+    let lines = change.document.getText().split(/\r?\n/g);
+    lines.forEach((line, i) => {
+        let index = line.indexOf('typescript');
+        if (index >= 0) {
+            diagnostics.push({
+                severity: vscode_languageserver_1.DiagnosticSeverity.Warning,
+                range: {
+                    start: { line: i, character: index },
+                    end: { line: i, character: index + 10 }
+                },
+                message: `${line.substr(index, 10)} should be spelled TypeScript`,
+                source: 'ex'
+            });
+        }
+    });
+    // Send the computed diagnostics to VS Code.
+    connection.sendDiagnostics({ uri: change.document.uri, diagnostics });
+});
+// Listen on the connection
+connection.listen();
+//# sourceMappingURL=server.js.map
